0cbe7df31c5c290056a2862f4eaafdff762f7858
fredericoaalcaria <73961504+fredericoaalcaira@users.noreply.github.com>
2022-01-30 21:11:21 +0000

    jogo humano

--- jogo.lisp
+++ jogo.lisp
@@ -54,8 +54,8 @@
    (format t " ~%|           JOGO DO BLOKUS            |")
    (format t " ~%|                                     |")
    (format t " ~%|        Qual o tempo limite?         |")
+   (format t " ~%|        Entre 1000 a 20000 ms.       |")
    (format t " ~%|                                     |")
-   (format t " ~%|     > 0 - Tempo (milisegundos)      |")
    (format t " ~%|       0 - Voltar                    |")
    (format t " ~%|                                     |")
    (format t " ~%|_____________________________________|")
@@ -87,47 +87,154 @@
   (progn (mostrar-menu-tempo-limite)
     (let ((option (read)))
       (cond
-      ((or (not (numberp option)) (< option 0)) (format t "Opção inválida!") (menu-limite-tempo jogador))
+      ((or (not (numberp option)) (< option 1000) (> option 20000)) (format t "~% Opção inválida!~%") (menu-limite-tempo jogador))
       ((eq option '0) (menu-escolher-jogador))
-      (T (jogo-teste))))))
+      (T (cond 
+        ((null jogador) (registar-cabecalho "Computador VS Computador" option ) (jogo-pc-vs-pc option))
+        (T (registar-cabecalho "Humano VS Computador" option ) (jogo-humano-vs-pc jogador option))))))))
 
 
-(defun jogo-teste (tempo-limite)
+(defun jogo-humano-vs-pc (jogador-humano tempo-limite)
   (let ((jogador 1)
         (adversario 2)
         (no-atual (no-inicial))
         (profundidade (profundidade-max-para-tempo tempo-limite)))
 
     (loop do (progn
-              (reiniciar-valores)
-              (definir-limite-tempo tempo-limite)
-              (let ((valor (time (alfabeta
-                            no-atual
-                            (criar-f-sucessores jogador adversario)
-                            (criar-f-utilidade jogador adversario) profundidade))))
-                (progn
-                  (format t "Turno do Jogador ~a ~% ------------------- ~%" jogador)
-                 (if valor
-                     (progn
-                      (setf no-atual (obter-melhor-jogada))
-                      
-                      (mostrar-no no-atual)
-                      (mostrar-estatisticas valor (obter-nos-analisados) (obter-cortes-alfa) (obter-cortes-beta) (obter-limite-tempo-alcancado)))
-                     (format t "Jogador ~a não conseguiu efetuar jogada ~% ------------------- ~% ~%" jogador))
-
+      (If (= jogador jogador-humano)
+        (let ((jogada (jogada-humano no-atual jogador)))
+          (if (not (null jogada))
+            (setf no-atual jogada)
+          )
+        )
+        (let ((jogada (jogada-computador no-atual jogador adversario profundidade tempo-limite)))
+          (if (not (null jogada))
+            (setf no-atual jogada)
+          )
+        ))             
                  (let ((temp jogador))
                    (setf jogador adversario)
-                   (setf adversario temp)))))
+                   (setf adversario temp)))
               while (not (and (null (sucessores no-atual 1 (operadores)))
                               (null (sucessores no-atual 2 (operadores))))))
-             (mostrar-pontuacoes (no-estado no-atual))))
+             (mostrar-pontuacoes (no-estado no-atual))
+             ))
+
+(defun jogada-computador (no jogador adversario profundidade tempo-limite)
+  (progn
+    (reiniciar-valores)
+    (definir-limite-tempo tempo-limite)
+    (let ((valor (alfabeta
+                    no
+                    (criar-f-sucessores jogador adversario)
+                    (criar-f-utilidade jogador adversario) profundidade)))
+                (progn
+                  (mostrar-turno-jogador jogador)
+                (if valor
+                  (progn                    
+                    (mostrar-no (obter-melhor-jogada))
+                      (mostrar-estatisticas valor (obter-nos-analisados) (obter-cortes-alfa) (obter-cortes-beta) (obter-limite-tempo-alcancado)))
+                     (mostrar-jogador-passou jogador))))
+                     (obter-melhor-jogada))
+)             
+
+(defun jogada-humano (no jogador)
+  (let ((peca nil)
+      (x nil)
+      (y nil))
+    (progn
+      (format t " ~% Escolha uma ação: ")
+      (format t " ~% ")
+      (format t " ~% 1 - peça A")
+      (format t " ~% 2 - peça B")
+      (format t " ~% 3 - peça C-H")
+      (format t " ~% 4 - peça C-V")
+      (format t " ~% 0 - Passar o turno")
+      (let ((option (read)))
+        (cond
+          ((eq option '1) (setf peca 'peca-a))
+          ((eq option '2) (setf peca 'peca-b))
+          ((eq option '3) (setf peca 'peca-c-h))
+          ((eq option '4) (setf peca 'peca-c-v))
+          ((eq option '0) nil)))
+      (if (not (tem-peca peca (estado-pecas-jogador (no-estado no) jogador)))      
+        (progn
+          (format t " ~% A ~a não está disponivel!" peca)
+          (jogada-humano no jogador)
+        )
+      )
+      (format t " ~% Escolha uma linha [0,13]: ")
+      (let ((option (read)))
+        (setf x option))
+      (format t " ~% Escolha uma coluna [0,13]: ")
+      (let ((option (read)))
+        (setf y option))
+      (let ((casas-ocupadas (peca-casas-ocupadas x y (funcall peca))))
+        (if (valida-casas (estado-tabuleiro (no-estado no)) casas-ocupadas jogador)
+          (criar-no (criar-novo-estado (no-estado no) casas-ocupadas peca jogador) nil)
+          (progn
+            (format t " ~% Não é possivel jogar a ~a na posição ~a ~a!" peca x y)
+            (jogada-humano no jogador)
+          )
+        )
+      )
+        )))
+
+(defun jogo-pc-vs-pc (tempo-limite)
+  (let ((jogador 1)
+        (adversario 2)
+        (no-atual (no-inicial))
+        (profundidade (profundidade-max-para-tempo tempo-limite)))
+
+    (loop do (progn
+              (let ((jogada (jogada-computador no-atual jogador adversario profundidade tempo-limite)))
+                (if (not (null jogada))
+                  (setf no-atual jogada)
+                )
+              )               
+                 (let ((temp jogador))
+                   (setf jogador adversario)
+                   (setf adversario temp)))
+              while (not (and (null (sucessores no-atual 1 (operadores)))
+                              (null (sucessores no-atual 2 (operadores))))))
+             (mostrar-pontuacoes (no-estado no-atual))
+             ))
+
+(defun mostrar-jogador-passou(jogador)
+  (format t "Jogador ~a não conseguiu efetuar jogada ~% ------------------- ~% ~%" jogador)
+  (registar-jogador-passou jogador)
+)
+
+(defun registar-jogador-passou(jogador)
+  (with-open-file (file (diretorio-resultados) :direction :output :if-exists :append :if-does-not-exist :create)
+  (format file "Jogador ~a não conseguiu efetuar jogada ~% ------------------- ~% ~%" jogador))
+)
 
 (defun mostrar-estatisticas (melhor-valor nos-analisados cortes-alfa cortes-beta limite-tempo-alcancado)
   (format t "Melhor valor: ~a ~%" melhor-valor)
   (format t "Número nós analisados: ~a ~%" nos-analisados)
   (format t "Número cortes-alfa: ~a ~%" cortes-alfa)
   (format t "Número cortes-beta: ~a ~%" cortes-beta)
-  (format t "Limite de tempo alcançado: ~a ~% ~%" (if limite-tempo-alcancado "Sim" "Não")))
+  (format t "Limite de tempo alcançado: ~a ~% ~%" (if limite-tempo-alcancado "Sim" "Não"))
+  (registar-estatisticas melhor-valor nos-analisados cortes-alfa cortes-beta limite-tempo-alcancado))
+
+(defun mostrar-turno-jogador(jogador)
+  (format t "Turno do Jogador ~a ~% ------------------- ~%" jogador)
+  (registar-turno-jogador jogador)
+)
+
+(defun registar-turno-jogador(jogador)
+  (with-open-file (file (diretorio-resultados) :direction :output :if-exists :append :if-does-not-exist :create)
+  (format file "Turno do Jogador ~a ~% ------------------- ~%" jogador))
+)
+
+(defun registar-estatisticas (melhor-valor nos-analisados cortes-alfa cortes-beta limite-tempo-alcancado)
+  (with-open-file (file (diretorio-resultados) :direction :output :if-exists :append :if-does-not-exist :create)
+  (format file "Melhor valor: ~a ~%" melhor-valor)
+  (format file "Número nós analisados: ~a ~%" nos-analisados)
+  (format file "Número cortes-alfa: ~a ~%" cortes-alfa)
+  (format file "Número cortes-beta: ~a ~%" cortes-beta)
+  (format file "Limite de tempo alcançado: ~a ~% ~%" (if limite-tempo-alcancado "Sim" "Não"))))
 
 (defun mostrar-no (no)
   "Imprime no listener as informações do nó atual"
@@ -136,7 +243,8 @@
    (mostrar-tabuleiro (estado-tabuleiro (no-estado no)))
    (format t "Peças disponiveis: ~%")
    (format t "Jogador 1: ~a ~%" (estado-pecas-jogador (no-estado no) 1))
-   (format t "Jogador 2: ~a ~% ~% " (estado-pecas-jogador (no-estado no) 2))))
+   (format t "Jogador 2: ~a ~% ~% " (estado-pecas-jogador (no-estado no) 2))
+   (registar-no no)))
 
 (defun mostrar-pontuacoes (estado)
 "Imprime no listener as Pontuações"
@@ -144,6 +252,7 @@
         (format t "Pontuações: ~%")
         (format t "Jogador 1: ~a pontos ~%" (pontuacao estado 1))
         (format t "Jogador 2: ~a pontos ~% ~%" (pontuacao estado 2))
+        (registar-pontuacoes estado)
     )
 )
 
@@ -154,6 +263,16 @@
         (format file "Jogador 1: ~a pontos ~%" (pontuacao estado 1))
         (format file "Jogador 2: ~a pontos ~% ~%" (pontuacao estado 2)))
     )
+)
+
+(defun registar-cabecalho (tipo tempo)
+    (progn
+      (with-open-file (file (diretorio-resultados) :direction :output :if-exists :append :if-does-not-exist :create)
+        (format file "~%/////////////////////////////////////////////////////////////////////////~%")
+        (format file "Jogo: ~a ~%" tipo)
+        (format file "Tempo limite: ~a milisegundos ~%" tempo)
+        (format file "/////////////////////////////////////////////////////////////////////////~%")
+    ))
 )
 
 (defun registar-no (no)
